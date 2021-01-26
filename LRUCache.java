1dc8006e22cc04b69b5ab27d74868fce5f18b905
Ira Villanueva <ira_villanueva@dlsu.edu.ph>
2021-01-26 21:31:55 +0800

    added backend code

---
+++ LRUCache.java
@@ -0,0 +1,122 @@
+// Java program to implement LRU cache
+// using LinkedHashSet
+import java.util.*;
+
+class LRUCache {
+
+	Set<Integer> cache_list;
+    int capacity;
+    LinkedList<Integer> result;
+    public static int hit;
+    public static int miss;
+
+	public LRUCache(int capacity){
+		this.cache_list = new LinkedHashSet<Integer>(capacity);
+        this.capacity = capacity;
+        this.result = new LinkedList<Integer>(); 
+	}
+
+	// This function returns false if key is not
+	// present in cache. Else it moves the key to
+	// front by first removing it and then adding
+	// it, and returns true.
+	public boolean get(int key){
+		if (!cache_list.contains(key)){
+            miss++;
+            return false;
+        } else{
+            cache_list.remove(key);
+            cache_list.add(key);
+            hit++;
+            return true;
+        }
+	}
+
+	/* Refers key x with in the LRU cache */
+	public void refer(int key){	 
+		if (get(key) == false){
+            put(key);
+        }
+	}
+
+	// displays contents of cache in Reverse Order
+	public void display(){
+        //LinkedList<Integer> list = new LinkedList<>(cache);
+        
+        // The descendingIterator() method of java.util.LinkedList
+        // class is used to return an iterator over the elements
+        // in this LinkedList in reverse sequential order
+        Iterator<Integer> itr = result.iterator(); 
+        //System.out.println(result);
+
+        while (itr.hasNext())
+            System.out.print(itr.next() + " ");
+	}
+	
+	public void put(int key){
+        if (cache_list.size() == capacity) {
+                int firstKey = cache_list.iterator().next();
+                int index = result.indexOf(firstKey);
+
+                result.remove(index);
+                result.add(index, key);
+        
+                cache_list.remove(firstKey);
+                cache_list.add(key);
+                                
+        } else{
+            cache_list.add(key);
+            result.add(key);
+        }
+	}
+	
+	public static void main(String[] args)
+	{
+        Scanner scan = new Scanner(System.in);
+        
+
+        System.out.print("How many values would the memory contain? ");
+        int values = scan.nextInt();
+        System.out.print("Cache block? ");
+        int block = scan.nextInt();
+        System.out.print("Block size(word/s)? ");
+        int word = scan.nextInt();
+        System.out.print("Cache access time (ns)? ");
+        int cache_access = scan.nextInt();
+        System.out.print("Memory access time (ns)? ");
+        int memory_access = scan.nextInt();
+
+        //Initialize cache
+        LRUCache cache = new LRUCache(block);
+        
+        //get values from user
+        for (int i = 0 ; i < values; i++){
+            System.out.print("Enter value: ");
+            int data = scan.nextInt();
+            cache.refer(data);
+        }
+        scan.close();
+
+        float hit_rate = (float) hit/values;
+        float miss_rate = (float) miss/values;
+        
+        //Miss penalty (non-load thru)
+        int misspenalty = cache_access + word*memory_access + cache_access;
+
+        //Average access time
+        float average_access = ((hit_rate*cache_access) + (miss_rate*misspenalty));
+
+        //total access time
+        int total_acc = ((hit*word*cache_access) + ((cache_access+memory_access)*word*miss) + (miss*cache_access));
+
+        //Print output
+        System.out.println("Hit rate: " + hit + "/" + values);
+        System.out.println("Miss rate: " + miss + "/" + values);
+        System.out.println("Miss Penalty: " + misspenalty);
+        System.out.println("Average memory access time: " + average_access);
+        System.out.println("Total memory access time: " + total_acc);
+        System.out.print("Cache memory: ");
+		
+		cache.display();
+	}
+}
